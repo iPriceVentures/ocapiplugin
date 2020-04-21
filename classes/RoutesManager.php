74c256c95b2f1d9e97ee395d529d8e8205e9aa52
minhtran17 <tran.quoc.minh@ipricegroup.com>
2020-04-21 18:30:08 +0800

    Ability to create custom api endpoint

--- classes/RoutesManager.php
+++ classes/RoutesManager.php
@@ -11,10 +11,13 @@ class RoutesManager
 {
     private const ROUTE_PATH = __DIR__ . '/../routes.php';
     private const ROUTE_TEMPLATE_PATH = __DIR__ . '/../templates/route.tpl';
+    private const ROUTE_API_RESOURCE_TEMPLATE_PATH = __DIR__ . '/../templates/route_api_resource.tpl';
     private const ROUTE_TEMPLATE_PLACEHOLDERS = [
+        '%router_method%',
         '%base_endpoint%',
         '%controller_class%',
         '%options%',
+        '%middleware_string%',
     ];
 
     /**
@@ -31,6 +34,7 @@ class RoutesManager
     {
         $this->filesystem = $filesystem;
         $this->routeTemplate = $this->filesystem->get(self::ROUTE_TEMPLATE_PATH);
+        $this->routeApiResourceTemplate = $this->filesystem->get(self::ROUTE_API_RESOURCE_TEMPLATE_PATH);
     }
 
     public function syncRoutes(Collection $resources): bool
@@ -47,18 +51,35 @@ class RoutesManager
     private function compileRoute(Resource $resource): string
     {
         $replacements = [
+            $resource->router_method,
             $resource->base_endpoint,
             ApiControllersManager::getFullyQualifiedControllerClass($resource),
-            $this->getOptions($resource)
+            $this->getOptions($resource),
+            $this->getMiddleWareString($resource),
         ];
 
-        return str_replace(self::ROUTE_TEMPLATE_PLACEHOLDERS, $replacements, $this->routeTemplate);
+        return str_replace(self::ROUTE_TEMPLATE_PLACEHOLDERS, $replacements, $this->getRouteTemplate($resource));
+    }
+
+    private function getRouteTemplate(Resource $resource): string
+    {
+        $variable = sprintf('route%sTemplate', ucwords($resource->router_method));
+        return $this->$variable ?: $this->routeTemplate;
     }
 
     private function getOptions(Resource $resource): string
     {
         if ($resource->is_auth_required) {
             return "'middleware' => '" . Authenticate::class . "'";
+        }
+
+        return '';
+    }
+
+    private function getMiddleWareString(Resource $resource): string
+    {
+        if ($resource->is_auth_required) {
+            return sprintf("->middleware('%s')", Authenticate::class);
         }
 
         return '';
---
+++ updates/builder_table_update_ipricegroup_ocapiplugin_resources_add_route_action.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace IPriceGroup\OcApiPlugin\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateIpricegroupOcapipluginResourcesAddRouteAction extends Migration
+{
+    const TABLE_NAME = 'ipricegroup_ocapiplugin_resources';
+
+    public function up()
+    {
+        Schema::table(self::TABLE_NAME, function($table)
+        {
+            $table->string('router_method', 20)->default('apiResource');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table(self::TABLE_NAME, function($table)
+        {
+            $table->dropColumn('router_method');
+        });
+    }
+}
